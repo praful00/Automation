3c189afe1cb8bf66d086d0f19aba158d71580da5
praful00 <111114639+praful00@users.noreply.github.com>
2022-10-18 16:07:08 +0530

    Update Registrationdata.py
    test demo

--- YUVICARE/Testdata/Registrationdata.py
+++ YUVICARE/Testdata/Registrationdata.py
@@ -1,4 +1,4 @@
 class RegistrationData:
-    test_registration_data=[{"userdid":"vspmdemo","password":"Expert#100","salutation":"1","firstname":"parag"
+    test_registration_data=[{"userdid":"vspmdemo","password":"Expert#100","salutation":"1","firstname":"shubham"
                              ,"lastname":"raut","contactno":"0000000000","address":"nagpur","entermonth":"Jan","enteryear":"1993"
                              }]
