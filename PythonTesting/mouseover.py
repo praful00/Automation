4eda8f5041637a209d14180f0688d748b3904b16
prafuls <prafulshinde00777@gmail.com>
2022-10-21 17:03:16 +0530

    push

--- PythonTesting/mouseover.py
+++ PythonTesting/mouseover.py
@@ -7,7 +7,7 @@ driver=webdriver.Chrome()
 driver.get("https://rahulshettyacademy.com/AutomationPractice/")
 driver.maximize_window()
 
-action=ActionChains(driver)
+action=ActionChains(driver)  
 webelement=driver.find_element(By.ID,"mousehover")
 action.move_to_element(webelement).perform()
 #webelement1=driver.find_element((By.LINK_TEXT,"Reload"))
